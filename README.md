8aecf8b01bb727b5b54661698c49324768148789
Will Farrington <wfarr@github.com>
2012-10-02 15:44:38 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Puppet inifile for Boxen
+
+FIXME: update me bro
+
---
+++ lib/puppet/provider/ini_setting/ruby.rb
@@ -0,0 +1,19 @@
+require File.expand_path('../../../util/ini_file', __FILE__)
+
+Puppet::Type.type(:ini_setting).provide(:ruby) do
+  def exists?
+    ini_file.get_value(resource[:section], resource[:setting]) == resource[:value].to_s
+  end
+
+  def create
+    ini_file.set_value(resource[:section], resource[:setting], resource[:value])
+    ini_file.save
+    @ini_file = nil
+  end
+
+
+  private
+  def ini_file
+    @ini_file ||= Puppet::Util::IniFile.new(resource[:path])
+  end
+end
